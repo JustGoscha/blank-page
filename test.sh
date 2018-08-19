96ff1000958c87d4784105f929c8ca2efff01aac
justgoscha <justgoscha@googlemail.com>
2018-08-19 12:21:54 +0200

    Fix coffee tests

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # run the js through babel so the module systems are commonjs for mocha
 mkdir ./test/js
-find ./js/*.js -type f -exec sh -c './node_modules/babel-cli/bin/babel.js "$0" > test/"$0"' {} ';'
+#find ./js/*.js -type f -exec sh -c './node_modules/babel-cli/bin/babel.js "$0" > test/"$0"' {} ';'
 # ./node_modules/mocha/bin/mocha ./test/main.coffee --compilers js:babel-core/register --compilers coffee:./coffee-babel --reporter spec
 ./node_modules/mocha/bin/mocha ./test/main.coffee --compilers coffee:./coffee-babel --reporter spec
 
